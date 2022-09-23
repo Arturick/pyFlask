0a14c3c7734c159e36ebdc164a22d9e260321944
_Artur_chiK_ <48881349+Arturick@users.noreply.github.com>
2022-09-23 23:35:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -39,6 +39,5 @@ def addLead():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0",port=5000)
+    
